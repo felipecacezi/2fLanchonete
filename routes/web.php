0db92ac67212c636e739b83af9abc3744a548122
Felipe Cacezi <felipecacezi@gmail.com>
2024-06-20 07:53:20 -0300

    feat: inicio implementação de categorias

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
@@ -23,6 +24,8 @@ Route::middleware([
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
     });
 
 });
