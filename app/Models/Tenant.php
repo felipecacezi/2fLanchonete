963e2101063b5247dab1291f15135189328e5144
Felipe Cacezi <felipecacezi@gmail.com>
2024-11-21 18:27:29 -0300

    feat: inicio criação das seeds

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
@@ -8,5 +9,5 @@ use Stancl\Tenancy\Database\Concerns\HasDomains;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    use HasDatabase, HasDomains;
+    use HasFactory, HasDatabase, HasDomains;
 }
